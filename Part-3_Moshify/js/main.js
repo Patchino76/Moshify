765cfd4b7558f66fc7f64aa2008098d4f3bbc2dd
Svetoslav Lyubenov <patchino76@gmail.com>
2023-07-04 16:47:54 +0300

    36-footer

--- Part-3_Moshify/js/main.js
+++ Part-3_Moshify/js/main.js
@@ -2,6 +2,8 @@ const collapsibles = document.querySelectorAll('[class*="collapsible"]');
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
+    // this.classList.toggle("collapsible__chevron");
+    // this.classList.toggle("collapsible__content");
     console.log("clicked");
   })
 );
